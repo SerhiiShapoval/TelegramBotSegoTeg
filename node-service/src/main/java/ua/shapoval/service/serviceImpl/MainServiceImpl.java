5ddb825366d3ea585f134d111d93302f30789f3d
Admin <shapovals1991@gmail.com>
2023-06-19 16:26:31 +0300

    jpa service

--- node-service/src/main/java/ua/shapoval/service/serviceImpl/MainServiceImpl.java
+++ node-service/src/main/java/ua/shapoval/service/serviceImpl/MainServiceImpl.java
@@ -4,7 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import ua.shapoval.entity.AppUser;
 import ua.shapoval.entity.UpdateData;
+import ua.shapoval.entity.UserState;
+import ua.shapoval.repository.AppUserRepository;
 import ua.shapoval.repository.UpdateDataRepository;
 import ua.shapoval.service.MainService;
 
@@ -16,13 +20,31 @@ import javax.transaction.Transactional;
 public class MainServiceImpl implements MainService {
   // private final UpdateData data;
     private final UpdateDataRepository repository;
+    private final AppUserRepository userRepository;
 
     @Override
     @Transactional
     public void processTextMessage(Update update) {
+     var appUser = findOrSaveAppUser(update.getMessage().getFrom());
         repository.save(UpdateData.builder()
                         .event(update)
                          .build());
 
+    }
+
+
+    private AppUser findOrSaveAppUser(User userTelegram){
+      var persistentAppUser = userRepository.findAppUserByTelegramUserId(userTelegram.getId());
+      if (persistentAppUser == null){
+        return userRepository.save(AppUser.builder()
+                        .telegramUserId(userTelegram.getId())
+                        .userName(userTelegram.getUserName())
+                        .firstName(userTelegram.getFirstName())
+                        .lastName(userTelegram.getLastName())
+                        .isActive(true)
+                        .userState(UserState.BASIC_STATE)
+                .build());
+      }
+      return userRepository.save(persistentAppUser);
     }
 }
